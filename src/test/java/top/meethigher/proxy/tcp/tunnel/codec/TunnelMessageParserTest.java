d7647f133483a4cdb97ec958556cbe0896dade7c
meethigher <meethigher@qq.com>
2025-05-24 15:39:20 +0800

    test: 补充编解码单元测试

--- src/test/java/top/meethigher/proxy/tcp/tunnel/codec/TunnelMessageParserTest.java
+++ src/test/java/top/meethigher/proxy/tcp/tunnel/codec/TunnelMessageParserTest.java
@@ -5,6 +5,7 @@ import io.vertx.core.buffer.Buffer;
 import org.junit.Test;
 
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class TunnelMessageParserTest {
 
@@ -16,7 +17,8 @@ public class TunnelMessageParserTest {
         Buffer buffer3 = TunnelMessageCodec.encode((short) 3, "third message".getBytes(StandardCharsets.UTF_8));
         Buffer buffer4 = TunnelMessageCodec.encode((short) 4, "forth message".getBytes(StandardCharsets.UTF_8));
 
-        Buffer buffer = Buffer.buffer().appendBuffer(buffer1).appendBuffer(buffer2).appendBuffer(buffer3).appendBuffer(buffer4);
+        final Buffer bufferOrigin = Buffer.buffer().appendBuffer(buffer1).appendBuffer(buffer2).appendBuffer(buffer3).appendBuffer(buffer4);
+        Buffer buffer = bufferOrigin.copy();
         Handler<Buffer> handler = buf -> {
             TunnelMessageCodec.DecodedMessage decodedMessage = TunnelMessageCodec.decode(buf);
             System.out.println(new String(decodedMessage.body));
@@ -24,6 +26,27 @@ public class TunnelMessageParserTest {
 
         TunnelMessageParser parser = new TunnelMessageParser(handler, null);
 
+        // 一条消息完整写出
         parser.handle(buffer);
+
+        for (int i = 0; i < 10000; i++) {
+            // 随机长度消息写出
+            Buffer tBuf = bufferOrigin.copy();
+            System.out.println("start " + tBuf.length());
+            int index = 0;
+            while (true) {
+                int lastIndex = tBuf.length();
+                if (index >= lastIndex) {
+                    break;
+                }
+                int endIndex = ThreadLocalRandom.current().nextInt(index, lastIndex + 1);
+                parser.handle(tBuf.getBuffer(index, endIndex));
+                System.out.println(index + "--" + endIndex);
+                index = endIndex;
+
+            }
+            System.out.println("end");
+        }
+
     }
 }
